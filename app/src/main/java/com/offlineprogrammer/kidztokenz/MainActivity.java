5f099e80514f209c2368716b844d101bbf79bcd8
offlineProgrammer <mmalaka@gmail.com>
2020-02-23 20:52:28 -0800

    monsters

--- app/src/main/java/com/offlineprogrammer/kidztokenz/MainActivity.java
+++ app/src/main/java/com/offlineprogrammer/kidztokenz/MainActivity.java
@@ -3,12 +3,17 @@ package com.offlineprogrammer.kidztokenz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView kidzList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        kidzList = findViewById(R.id.kidz_list);
+
     }
 }
