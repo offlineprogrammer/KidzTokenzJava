e8116a6d932e0c72143d11a3e8fe3ce520493fdb
offlineProgrammer <mmalaka@gmail.com>
2020-02-25 22:28:45 -0800

    Kid Class

---
+++ app/src/main/java/com/offlineprogrammer/kidztokenz/Kid.java
@@ -0,0 +1,18 @@
+package com.offlineprogrammer.kidztokenz;
+
+public class Kid {
+    private String kidName;
+
+    public Kid(String kidName) {
+        this.kidName = kidName;
+    }
+
+    @NonNull
+    public String getKidName() {
+        return simpleText;
+    }
+
+    public void setKidName(@NonNull final String kidName) {
+        this.kidName = kidName;
+    }
+}
