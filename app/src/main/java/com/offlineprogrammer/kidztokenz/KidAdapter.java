f334bef35fe73e4ef98d720fcb80fc048b753692
offlineProgrammer <mmalaka@gmail.com>
2020-02-26 20:59:22 -0800

    Kid View Handler

--- app/src/main/java/com/offlineprogrammer/kidztokenz/KidAdapter.java
+++ app/src/main/java/com/offlineprogrammer/kidztokenz/KidAdapter.java
@@ -1,4 +1,27 @@
 package com.offlineprogrammer.kidztokenz;
 
-public class KidAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class KidAdapter extends RecyclerView.Adapter {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        final View view = LayoutInflater.from(parent.getContext()).inflate(viewType, parent, false);
+        return new KidViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
---
+++ app/src/main/java/com/offlineprogrammer/kidztokenz/KidViewHolder.java
@@ -0,0 +1,12 @@
+package com.offlineprogrammer.kidztokenz;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class KidViewHolder extends RecyclerView.ViewHolder {
+    public KidViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
