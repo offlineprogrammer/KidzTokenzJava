504026fc56f532ecd4d8d5dd2d0ae9416cb07cd3
offlineProgrammer <mmalaka@gmail.com>
2020-02-27 10:18:21 -0800

    Kidz List

--- app/src/main/java/com/offlineprogrammer/kidztokenz/MainActivity.java
+++ app/src/main/java/com/offlineprogrammer/kidztokenz/MainActivity.java
@@ -1,19 +1,40 @@
 package com.offlineprogrammer.kidztokenz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
-    ListView kidzList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        KidAdapter adapter = new KidAdapter(generateKidList());
        // kidzList = findViewById(R.id.kidz_list);
 
+        RecyclerView recyclerView = findViewById(R.id.kidz_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    private List<Kid> generateKidList() {
+        List<Kid> kidzList = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++)
+            kidzList.add(new Kid(String.format(Locale.US, "This is item %d", i)));
+
+        return kidzList;
     }
 }
