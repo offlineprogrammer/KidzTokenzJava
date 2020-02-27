907a330299fd6b1abb097a3ff6f7cd237503caa5
offlineProgrammer <mmalaka@gmail.com>
2020-02-26 21:22:32 -0800

    Kid View Handler

--- app/src/main/java/com/offlineprogrammer/kidztokenz/KidAdapter.java
+++ app/src/main/java/com/offlineprogrammer/kidztokenz/KidAdapter.java
@@ -7,7 +7,18 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class KidAdapter extends RecyclerView.Adapter {
+    private List<Kid> models;
+
+    public KidAdapter(final List<Kid> viewModels) {
+        if (viewModels != null) {
+            this.models.addAll(viewModels);
+        }
+    }
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -17,11 +28,17 @@ public class KidAdapter extends RecyclerView.Adapter {
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ((KidViewHolder) holder).bindData(models.get(position));
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return models.size();
+    }
+
+    @Override
+    public int getItemViewType(final int position) {
+        return R.layout.kid_itemview;
     }
 }
--- app/src/main/java/com/offlineprogrammer/kidztokenz/KidViewHolder.java
+++ app/src/main/java/com/offlineprogrammer/kidztokenz/KidViewHolder.java
@@ -1,12 +1,18 @@
 package com.offlineprogrammer.kidztokenz;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class KidViewHolder extends RecyclerView.ViewHolder {
+    private TextView kidNameTextView;
     public KidViewHolder(@NonNull View itemView) {
         super(itemView);
+    }
+
+    public void bindData(final Kid viewModel) {
+        kidNameTextView.setText(viewModel.getKidName());
     }
 }
