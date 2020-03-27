e71578a10d292983f264a536bd3ffd76301cf971
offlineProgrammer <mmalaka@gmail.com>
2020-03-27 12:06:51 -0700

    Update Token Image

--- app/src/main/java/com/offlineprogrammer/kidztokenz/KidActivity.java
+++ app/src/main/java/com/offlineprogrammer/kidztokenz/KidActivity.java
@@ -1,5 +1,6 @@
 package com.offlineprogrammer.kidztokenz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
@@ -13,6 +14,11 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class KidActivity extends AppCompatActivity {
 
     private static final String TAG = "KidActivity";
@@ -20,6 +26,7 @@ public class KidActivity extends AppCompatActivity {
     TextView kidNameTextView;
     ImageView tokenImageView;
     CardView tokenImageCard;
+    Kid selectedKid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,16 +49,16 @@ public class KidActivity extends AppCompatActivity {
 
         if (getIntent().hasExtra("selected_kid")) {
             Bundle data = getIntent().getExtras();
-            Kid kid = (Kid) data.getParcelable("selected_kid");
+            selectedKid = (Kid) data.getParcelable("selected_kid");
            // Kid kid = getIntent().getParcelableExtra("selected_kid");
-            Log.i(TAG, "onCreate: " + kid.toString());
-            Log.i(TAG, "onCreate: " + kid.getTokenImage());
-            Log.i(TAG, "onCreate: " + kid.getFirestoreId());
-            Log.i(TAG, "onCreate: " + kid.getUserFirestoreId());
-            Log.i(TAG, "onCreate STRING: " + kid.toString());
-            kidImageView.setImageResource(kid.getMonsterImage());
-            kidNameTextView.setText(kid.getKidName());
-            tokenImageView.setImageResource(kid.getTokenImage());
+            Log.i(TAG, "onCreate: " + selectedKid.toString());
+            Log.i(TAG, "onCreate: " + selectedKid.getTokenImage());
+            Log.i(TAG, "onCreate: " + selectedKid.getFirestoreId());
+            Log.i(TAG, "onCreate: " + selectedKid.getUserFirestoreId());
+            Log.i(TAG, "onCreate STRING: " + selectedKid.toString());
+            kidImageView.setImageResource(selectedKid.getMonsterImage());
+            kidNameTextView.setText(selectedKid.getKidName());
+            tokenImageView.setImageResource(selectedKid.getTokenImage());
         }
     }
 
@@ -63,10 +70,34 @@ public class KidActivity extends AppCompatActivity {
             if(resultCode == Activity.RESULT_OK){
                 int result=data.getIntExtra("Image",0);
                 tokenImageView.setImageResource(result);
+                updateKidTokenImage(result);
             }
             if (resultCode == Activity.RESULT_CANCELED) {
                 //Write your code if there's no result
             }
         }
     }//onActivityResult
+
+    private void updateKidTokenImage(int newTokenImage) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        DocumentReference selectedKidRef = db.collection("users").document(selectedKid.getUserFirestoreId()).collection("kidz").document(selectedKid.getFirestoreId());
+
+// Set the "isCapital" field of the city 'DC'
+        selectedKidRef
+                .update("tokenImage", newTokenImage)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully updated!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error updating document", e);
+                    }
+                });
+
+    }
 }
