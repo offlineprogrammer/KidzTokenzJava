b56681806d4a040c3b0a180ff859a3d7a3895bf4
offlineProgrammer <mmalaka@gmail.com>
2020-04-18 18:53:30 -0700

    Firebase Photo on kid page

--- app/src/main/java/com/offlineprogrammer/KidzTokenz/task/TaskViewHolder.java
+++ app/src/main/java/com/offlineprogrammer/KidzTokenz/task/TaskViewHolder.java
@@ -1,5 +1,6 @@
 package com.offlineprogrammer.KidzTokenz.task;
 
+import android.net.Uri;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -8,6 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.offlineprogrammer.KidzTokenz.R;
+import com.squareup.picasso.Picasso;
 
 public class TaskViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     private TextView taskNameTextView;
@@ -22,8 +24,18 @@ public class TaskViewHolder extends RecyclerView.ViewHolder implements View.OnCl
     }
 
     public void bindData(final KidTask viewModel) {
+        Uri taskTokenImageUri = null;
         taskNameTextView.setText(viewModel.getTaskName());
-        taskImageView.setImageResource(viewModel.getTaskImage());
+        if (viewModel.getFirestoreImageUri() == null) {
+            taskImageView.setImageResource(viewModel.getTaskImage());
+        }
+        else {
+            taskTokenImageUri = Uri.parse(viewModel.getFirestoreImageUri());
+            Picasso.get().load(taskTokenImageUri)
+                    .placeholder(R.drawable.bekind)
+                    .into(taskImageView);
+        }
+
     }
 
     @Override
