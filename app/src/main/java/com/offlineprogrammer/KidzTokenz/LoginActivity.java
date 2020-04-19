585dfe0e93a020109cb66cf7f97a2eaae98f1e27
offlineProgrammer <mmalaka@gmail.com>
2020-04-19 15:06:44 -0700

    Login Activity Style

--- app/src/main/java/com/offlineprogrammer/KidzTokenz/LoginActivity.java
+++ app/src/main/java/com/offlineprogrammer/KidzTokenz/LoginActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.common.SignInButton;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        signInButton.setColorScheme(SignInButton.COLOR_LIGHT);
     }
 }
